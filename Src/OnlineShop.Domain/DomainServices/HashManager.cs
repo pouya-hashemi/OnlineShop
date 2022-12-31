6b7239ae61a43881e90a9106c9055938904838b2
Pouya Hashemi <Pouya.hashemi.dev@gmail.com>
2022-12-31 16:52:29 +0330

    Hash initial

--- Src/OnlineShop.Domain/DomainServices/HashManager.cs
+++ Src/OnlineShop.Domain/DomainServices/HashManager.cs
@@ -1,6 +1,22 @@
-﻿namespace OnlineShop.Domain.DomainServices;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace OnlineShop.Domain.DomainServices;
 
 public class HashManager
 {
-     
+    public string CreateHash(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            throw new ArgumentException($"'{nameof(text)}' cannot be null or whitespace.", nameof(CreateHash));
+        }
+
+        using (SHA256 mySha256 = SHA256.Create())
+        {
+            var hash = mySha256.ComputeHash(Encoding.ASCII.GetBytes(text));
+            var hashStr = Encoding.ASCII.GetString(hash);
+            return hashStr;
+        }
+    }
 }
